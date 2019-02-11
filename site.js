ab7ee0d1000ccf1d932362a373554ef630ec0641
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2019-02-10 21:39:56 -0600

    Add event listeners for when users focus on an input field

---
+++ site.js
@@ -0,0 +1,31 @@
+// To ensure that if the user's browser does not have JavaScript or can't handle
+// it, will not run any JavaScript
+(function(){
+  if (!('querySelector' in document && 'addEventListener' in document)){
+    return;
+  }
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+  console.log('LOADED DOM');
+});
+
+var fname_input = document.querySelector('#fname');
+fname_input.addEventListener('focus', function(){
+  console.log('Focused');
+});
+
+var lname_input = document.querySelector('#lname');
+lname_input.addEventListener('focus', function(){
+  console.log('Focused');
+});
+
+var email_input = document.querySelector('#email');
+email_input.addEventListener('focus', function(){
+  console.log('Focused');
+});
+
+var phone_input = document.querySelector('#phone');
+phone_input.addEventListener('focus', function(){
+  console.log('Focused');
+});
