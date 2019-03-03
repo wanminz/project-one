b166518b9132f057d93643db5037b3e9e75a9338
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2019-03-03 16:26:14 -0600

    Utilize focus method for input boxes on the form

--- site.js
+++ site.js
@@ -1,6 +1,7 @@
 // Using global form
 "use strict";
 
+console.log('Starting');
 // Function to check if the browser is able to run javascript
 (function(){
   // If javascript can't be ran, exits out of function
@@ -8,13 +9,26 @@
   {
     return;
   }
+
+  console.log('hello');
   document.addEventListener('DOMContentLoaded', function(){
     // Link DOM elements
     var signup_form = document.querySelector('#dogform');
     var name_input = document.querySelector('#name');
     var email_input = document.querySelector('#email');
-    var numinput = document.querySelector('#phone');
+    var num_input = document.querySelector('#phone');
     var submitbtn = document.querySelector('#signup');
+
+    // Using focus method on user input boxes
+    document.getElementById('name_input').focus();
+    document.getElementById('email_input').focus();
+    document.getElementById('num_input').focus();
+
+    console.log('helo');
+
+    document.getElementById('submitbtn').disabled = true;
+
+
   });
 
 
